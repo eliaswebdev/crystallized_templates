5eb96c68d00894b07175859388fbe7a4721f4a49
Elias Ferreira Junior <eliaswebdev@gmail.com>
2018-07-10 15:02:55 -0300

    quick commit

--- lib/generators/rails/templates/templates_generator.rb
+++ lib/generators/rails/templates/templates_generator.rb
@@ -2,6 +2,6 @@ class Rails::TemplatesGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   def create_initializer_file
-    puts 'downloading...'
+    puts 'Downloading....'
   end
 end
