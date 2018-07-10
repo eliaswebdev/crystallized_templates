3d2a6325e09e20921ef04b561fa732b7ae7ce631
Elias Ferreira Junior <eliaswebdev@gmail.com>
2018-07-10 14:37:28 -0300

    quick commit

--- lib/crystallized_templates.rb
+++ lib/crystallized_templates.rb
@@ -3,5 +3,6 @@ require "crystallized_templates/version"
 module CrystallizedTemplates
   def say!
     puts 'hello'
+    puts "mkdir -p lib/templates/erb/scaffold && \ cp $(bundle show railties)/lib/rails/generators/erb/scaffold/templates/* lib/templates/erb/scaffold"
   end
 end
